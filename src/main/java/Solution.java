64fbb45e25cd6374d16036058b561ffb42502a38
relzet <relzet69@gmail.com>
2017-03-27 01:24:08 +0300

    some fixes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,32 +1,15 @@
 import java.io.IOException;
 
 public class Solution {
-    static int IMAGE_DPI = 150;
 
     public static void main(String[] args) throws IOException {
-//        PDDocument document = PDDocument.load(new File("D:\\Acura.pdf"));
-//        PDDocumentOutline outline =  document.getDocumentCatalog().getDocumentOutline();
-//        printBookmark(outline, "");
-//
-//        PDFTextStripper pdfStripper = new PDFTextStripper();
 
-
-//        document.close();
 
         PDFManager manager = new PDFManager("D:\\pdf\\Acura.pdf");
-        manager.toText();
+        manager.PdfToText();
+        manager.saveBookmarks();
 
 
     }
 
-//    private static void printBookmark(PDOutlineNode bookmark, String indentation) throws IOException
-//    {
-//        PDOutlineItem current = bookmark.getFirstChild();
-//        while (current != null)
-//        {
-//            System.out.println(indentation + current.getTitle());
-//            printBookmark(current, indentation + "    ");
-//            current = current.getNextSibling();
-//        }
-//    }
 }
