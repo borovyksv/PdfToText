20610d47973143bce6a7976e3720f8aed0a3055f
relzet <relzet69@gmail.com>
2017-06-09 11:42:51 +0300

    updated upload form

--- src/main/java/com/borovyksv/controller/MainController.java
+++ src/main/java/com/borovyksv/controller/MainController.java
@@ -5,12 +5,19 @@ import com.borovyksv.mongo.pojo.ProgressStatus;
 import com.borovyksv.mongo.repository.ConvertedDocumentRepository;
 import com.borovyksv.mongo.repository.DocumentWithTextPagesRepository;
 import com.borovyksv.mongo.repository.ProgressStatusRepository;
+import com.borovyksv.service.FileService;
+import com.borovyksv.sql.VehicleRepository;
+import com.borovyksv.sql.vehicle.FrontendVehicle;
+import com.borovyksv.sql.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -23,13 +30,11 @@ public class MainController {
   @Autowired
   DocumentWithTextPagesRepository documentWithTextPagesRepository;
   @Autowired
-  ServletContext servletContext;
+  VehicleRepository vehicleRepository;
 
+  @Autowired
+  FileService fileService;
 
-//  @RequestMapping(value = "/documents/{keyword}", method = RequestMethod.GET)
-//  public List<DocumentWithTextPages> search(@PathVariable( "keyword" ) String keyword) {
-//    return documentWithTextPagesRepository.search(keyword);
-//  }
 
   @RequestMapping(value = "/documents/progress", method = RequestMethod.GET)
   public List<ProgressStatus> progressStatus() {
@@ -40,6 +45,60 @@ public class MainController {
   public List<ConvertedDocument> convertedDocuments() {
     return convertedDocumentRepository.findAll();
   }
+
+  @RequestMapping(value = "/vehicles", method = RequestMethod.GET)
+  public List<FrontendVehicle> getVehicles(){
+    List<Vehicle> allAndGroup = vehicleRepository.findAllAndGroup();
+
+    List<FrontendVehicle> results = new ArrayList<>();
+    for (Vehicle vehicle : allAndGroup) {
+      FrontendVehicle fv = new FrontendVehicle();
+      fv.setVendor(vehicle.getModel_make());
+      fv.setModels(Arrays.asList(vehicle.getModel_name().split(", ")));
+      results.add(fv);
+    }
+    return results;
+  }
+
+  @RequestMapping(value = "/documents/store", method = RequestMethod.POST, consumes = "multipart/form-data")
+  public void handleFileUpload(@RequestPart("file") MultipartFile file, @RequestPart("info") FrontendVehicle info) {
+
+
+//        info = Arrays.asList(new Info("vendor", new String[]{"gms"}),
+//                new Info("model", new String[]{"acura", "denali"}),
+//                new Info("year", new String[]{"2007","2008"}));
+
+    System.out.println(info);
+    System.out.println(file.getOriginalFilename());
+
+//    Info vendor = info.get(0);
+//    Info model = info.get(1);
+//    Info year = info.get(2);
+//
+//    DocumentWithTextPages doc = new DocumentWithTextPages();
+//    doc.setName("Filename");
+//    doc.setVendor(vendor.getOptions()[0]);
+//    doc.setModel(Arrays.asList(model.getOptions()));
+//
+//    List<Integer> years = new ArrayList<>();
+//    for (String y : year.getOptions()) {
+//      years.add(Integer.valueOf(y));
+//    }
+//    doc.setYear(years);
+//    doc.setOptions(Collections.emptyList());
+//
+//
+//    DocumentWithTextPages saved = documentWithTextPagesRepository.save(doc);
+//    String id = saved.getId();
+//
+//    String result = " error";
+//    if (fileService.store(file, id)) {
+//      result = " uploaded";
+//    }
+
+  }
+
+
 
 
 }
